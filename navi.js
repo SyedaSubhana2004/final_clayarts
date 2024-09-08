3a6fd92cf11884d4b3ffa04d3d5c3c15635cc2ae
SyedaSubhana2004 <159274933+SyedaSubhana2004@users.noreply.github.com>
2024-09-08 12:07:33 -0700

    Delete navi.js

--- navi.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.getElementById('menu-icon');
-const navMenu = document.querySelector('.navmenu');
-
-menuIcon.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-    menuIcon.classList.toggle('bx-x'); // Toggle your menu icon class here
-});
